99810194109594dab4d4a8226481c8e3d9315745
SweetRabbit <sweetfullstack@gmail.com>
2018-09-08 19:13:51 +0800

    four insert

--- init.el
+++ init.el
@@ -1,24 +1,9 @@
-(package-initialize)
-
-(add-to-list 'load-path "~/.emacs.d/lisp/")
-
-;; Package Management
-;; -----------------------------------------------------------------
-(require 'init-packages)
-(require 'init-ui)
-(require 'init-keybindings)
-(require 'init-org)
-(require 'init-better-defaults)
-(require 'init-set-packages)
-
-(setq custom-file (expand-file-name "lisp/custom.el" user-emacs-directory))
-
-(load-file custom-file)
-
 ;;--------------------org管理emacs--------------------------
-;;(package-initialize)
 
-;;(require 'org-install)
-;;(require 'ob-tangle)
-;;(org-babel-load-file (expand-file-name "org-file-name.org" user-emacs-directory))
+(require 'org-install)
+(require 'ob-tangle)
+(org-babel-load-file (expand-file-name "sweet.org" user-emacs-directory))
+
+
+
 
---
+++ sweet.el
@@ -0,0 +1,16 @@
+(package-initialize)
+
+(add-to-list 'load-path "~/.emacs.d/lisp/")
+
+;; Package Management
+;; -----------------------------------------------------------------
+(require 'init-packages)
+(require 'init-ui)
+(require 'init-keybindings)
+(require 'init-org)
+(require 'init-better-defaults)
+(require 'init-set-packages)
+
+(setq custom-file (expand-file-name "lisp/custom.el" user-emacs-directory))
+
+(load-file custom-file)
